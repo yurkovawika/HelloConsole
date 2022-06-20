1e1675844fd1b34e92cf386a11f762c19b0a8544
Viktorinkiss <yurkovawika@gmail.com>
2022-06-20 16:11:18 +0500

    add number

--- Examples_003_Sum/Program.cs
+++ Examples_003_Sum/Program.cs
@@ -1,4 +1,4 @@
-﻿double numberA = 3;
-double numberB = 2;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+int result = numberA + numberB;
+Console.WriteLine(result);
